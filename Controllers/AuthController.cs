fa45b5905ad1fdb2d9a563be9c62fc2d831b4307
Mark Wetzel <mark@markwetzel.com>
2024-02-02 15:51:28 -0500

    Add controller stubs

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogOS.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        
+    }
+}
